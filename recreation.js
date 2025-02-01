766d6b119c732a4d7f0e6293629458e284cea52f
Everything-Design <88721702+Everything-Design@users.noreply.github.com>
2025-02-01 17:09:33 +0530

    Update recreation.js

--- recreation.js
+++ recreation.js
@@ -44,7 +44,7 @@
                         box-shadow: inset 0 1px 4px #0003;
                         transition: 500ms background-color ease;
                     }
-                    .ef-re-logo {  
+                    .ef-re-brand-logo {  
                         width: 100%;  
                         max-width: 1.2em;
                     }
@@ -86,7 +86,7 @@
                 </style>
             </div>
             <img src="https://cdn.prod.website-files.com/6435988d3f4500e4270fb5a3/6435988d3f450020310fb5ee_Everything%20Design%20Webclip.webp" 
-                loading="lazy" alt="everything flow logo" class="ef-re-logo">
+                loading="lazy" alt="everything flow logo" class="ef-re-brand-logo">
             <div class="ef-re-wrap">
                 <img src="https://cdn.prod.website-files.com/6435988d3f4500e4270fb5a3/679df272691f471fabf7ba5d_We%20do%20not%20own.svg" 
                     loading="lazy" alt="" class="abs-content-img">
